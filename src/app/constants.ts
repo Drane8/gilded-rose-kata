ab9785bbd93ded46112beb7f968ce86d68acbb3e
Daniel Garcia <danigarciaberzal@gmail.com>
2025-06-10 18:42:06 +0200

    refactor: remove magic string and created constants file

---
+++ src/app/constants.ts
@@ -0,0 +1,8 @@
+export const MIN_QUALITY = 0;
+export const MAX_QUALITY = 50;
+export const BACKSTAGE_DOUBLE_PRICE_DATE = 11;
+export const BACKSTAGE_TRIPLE_PRICE_DATE = 6;
+export const RECOMMENDED_SELLIN_DATE = 0;
+export const BRIE = "Aged Brie";
+export const BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+export const SULFURAS = "Sulfuras, Hand of Ragnaros";
--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -1,3 +1,5 @@
+import { BRIE, BACKSTAGE_PASSES, MIN_QUALITY, SULFURAS, MAX_QUALITY, BACKSTAGE_DOUBLE_PRICE_DATE, BACKSTAGE_TRIPLE_PRICE_DATE, RECOMMENDED_SELLIN_DATE } from "./constants";
+
 export class Item {
   constructor(
     public name: string,
@@ -6,23 +8,17 @@ export class Item {
   ) {}
 }
 
-const MIN_QUALITY = 0;
-const MAX_QUALITY = 50;
-const BACKSTAGE_DOUBLE_PRICE_DATE = 11;
-const BACKSTAGE_TRIPLE_PRICE_DATE = 6;
-const RECOMMENDED_SELLIN_DATE = 0;
-
 export class GildedRose {
   constructor(public items: Array<Item> = []) {}
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+        this.items[i].name != BRIE &&
+        this.items[i].name != BACKSTAGE_PASSES
       ) {
         if (this.items[i].quality > MIN_QUALITY) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          if (this.items[i].name != SULFURAS) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
@@ -30,7 +26,7 @@ export class GildedRose {
         if (this.items[i].quality < MAX_QUALITY) {
           this.items[i].quality = this.items[i].quality + 1;
           if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+            this.items[i].name == BACKSTAGE_PASSES
           ) {
             if (this.items[i].sellIn < BACKSTAGE_DOUBLE_PRICE_DATE) {
               if (this.items[i].quality < MAX_QUALITY) {
@@ -45,16 +41,16 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+      if (this.items[i].name != SULFURAS) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < RECOMMENDED_SELLIN_DATE) {
-        if (this.items[i].name != "Aged Brie") {
+        if (this.items[i].name != BRIE) {
           if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+            this.items[i].name != BACKSTAGE_PASSES
           ) {
             if (this.items[i].quality > MIN_QUALITY) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              if (this.items[i].name != SULFURAS) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
