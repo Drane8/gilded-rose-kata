09b8234860e990e98ec468a36e8f11b80a89dcad
Daniel Garcia <danigarciaberzal@gmail.com>
2025-06-10 19:04:38 +0200

    refactor: group by Backstage with early return

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -38,37 +38,34 @@ export class GildedRose {
         }
         return;
       }
-      
+
       if (item.name === BACKSTAGE_PASSES) {
         if (item.quality < MAX_QUALITY) {
           item.quality = item.quality + 1;
-          if (item.name === BACKSTAGE_PASSES) {
-            if (item.sellIn < BACKSTAGE_DOUBLE_PRICE_DATE) {
-              if (item.quality < MAX_QUALITY) {
-                item.quality = item.quality + 1;
-              }
+          if (item.sellIn < BACKSTAGE_DOUBLE_PRICE_DATE) {
+            if (item.quality < MAX_QUALITY) {
+              item.quality = item.quality + 1;
             }
-            if (item.sellIn < BACKSTAGE_TRIPLE_PRICE_DATE) {
-              if (item.quality < MAX_QUALITY) {
-                item.quality = item.quality + 1;
-              }
+          }
+          if (item.sellIn < BACKSTAGE_TRIPLE_PRICE_DATE) {
+            if (item.quality < MAX_QUALITY) {
+              item.quality = item.quality + 1;
             }
           }
         }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < RECOMMENDED_SELLIN_DATE) {
+          item.quality = item.quality - item.quality;
+        }
+        return;
       } else if (item.quality > MIN_QUALITY) {
         item.quality = item.quality - 1;
       }
 
       item.sellIn = item.sellIn - 1;
       if (item.sellIn < RECOMMENDED_SELLIN_DATE) {
-        if (item.name != BRIE) {
-          if (item.name != BACKSTAGE_PASSES) {
-            if (item.quality > MIN_QUALITY) {
-              item.quality = item.quality - 1;
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
+        if (item.quality > MIN_QUALITY) {
+          item.quality = item.quality - 1;
         }
       }
     });
